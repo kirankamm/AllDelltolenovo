fccfa305025a5aeca1f5505d53de4d0a01615255
kirankamm <118911532+kirankamm@users.noreply.github.com>
2023-11-07 23:45:22 +0530

    Add files via upload

---
+++ CssNotes.java
@@ -0,0 +1,131 @@
+Easy CSS Interview Questions
+
+Q1. What is the full form of CSS? 
+CSS stands for Cascading Style Sheets.
+ It is a technology developed by the World Wide Web Consortium or W3C. It was developed
+ to streamline the styling of webpages into a separate technology.
+ 
+ Q2. Why was CSS developed?
+CSS was first developed in 1997 as a way for web developers to define the visual appearance of the web pages that they were creating. It was intended to allow developers to separate the content and structure of a website’s code from the visual design, something that had not been possible prior to this time.
+
+The separation of structure and style allows HTML to perform more of the function that it was originally based on — the markup of content, without having to worry about the design and layout of the page itself, something commonly known as the “look and feel” of the page.
+ 
+ What is CSS?
+ 
+=CSS stands for Cascading Style Sheets
+=CSS describes how HTML elements are to be displayed on screen, paper, or in other media
+=CSS saves a lot of work. It can control the layout of multiple web pages all at once
+=External stylesheets are stored in CSS files
+ 
+ Q27. What is the property that is used for controlling image-scroll?
+The background-attachment property sets whether a background image scrolls with the rest of the page, or is fixed. Here is an example of a background-image that will not scroll with the page (fixed):
+
+body {
+  background-image: url("img_tree.gif");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
